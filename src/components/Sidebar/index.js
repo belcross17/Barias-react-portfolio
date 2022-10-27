a23348a7f41c8160d11c1ea3f90bd56c789cc2b5
Brayan <alfredochabes1@hotmail.com>
2022-10-27 13:20:07 -0400

    inserted logos and sub-log and home icon

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,14 +1,21 @@
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
 import LogoB from '../../assets/images/logo-b.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome } from '@fortawesome/free-solid-svg-icons'
 
 const Sidebar = () => (
     <div className='nav-bar'>
         <Link className='logo' to='/'>
             <img src={LogoB} alt="logo" />
-            <img src={LogoSubtitle} alt="logo" />
+            <img className="sub-logo" src={LogoSubtitle} alt="Brayan Arias" />
         </Link>
+        <nav>
+            <NavLink exact="true" activeclassname="active" to="/">
+                <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+            </NavLink>
+        </nav>
     </div>
     )
 
