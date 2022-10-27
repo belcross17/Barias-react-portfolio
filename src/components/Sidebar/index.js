7bba91a3b09a5cddb4aaa14f7acfc2fd503123b9
Brayan <alfredochabes1@hotmail.com>
2022-10-27 12:59:17 -0400

    added logos to website

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,5 +1,15 @@
+import { Link } from 'react-router-dom'
 import './index.scss'
+import LogoB from '../../assets/images/logo-b.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
 
-const Sidebar = () => <>Hello from sidebar</>
+const Sidebar = () => (
+    <div className='nav-bar'>
+        <Link className='logo' to='/'>
+            <img src={LogoB} alt="logo" />
+            <img src={LogoSubtitle} alt="logo" />
+        </Link>
+    </div>
+    )
 
 export default Sidebar;
