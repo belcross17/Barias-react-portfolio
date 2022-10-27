2ef16f137188997af8b6e416bd61461bf2ce9e0d
Brayan <alfredochabes1@hotmail.com>
2022-10-27 12:23:14 -0400

    added components and layout and new fonts

---
+++ src/components/Layout/index.js
@@ -0,0 +1,10 @@
+import Sidebar from '../Sidebar';
+import './index.scss';
+
+const Layout = () => {
+    return <>
+    <Sidebar />
+    </>
+}
+
+export default Layout;
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,5 @@
+import './index.scss'
+
+const Sidebar = () => <>Hello from sidebar</>
+
+export default Sidebar;
