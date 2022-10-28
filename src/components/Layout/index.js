067cd914d1d45ca87eb971616b9ac38fa612b6cb
Brayan <alfredochabes1@hotmail.com>
2022-10-28 13:58:01 -0400

    resize self picture

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -7,14 +7,10 @@ const Layout = () => {
     <div className="App">
         <Sidebar />
         <div className='page'>
-            <span className='tags top-tags'>&lt;body&gt;</span>
-
+            <span className='tags top-tags'></span>
             <Outlet />
-
             <span className='tags bottom-tags'>
-                &lt;body&gt;
-                <br />
-                <span className='bottom-tag-html'>&lt;/html&gt;</span>
+                <span className='bottom-tag-html'></span>
             </span>
 
         </div>
