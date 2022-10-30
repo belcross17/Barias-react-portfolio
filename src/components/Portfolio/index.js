f6acc3f3fe0b5ff640aeb29d5fa541c5e4f306db
Brayan <alfredochabes1@hotmail.com>
2022-10-29 22:42:40 -0400

    added screen size fix to 1200

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import Loader from "react-loaders";
 import runBuddy from '../../assets/images/run-buddy.png'
+import tourismApp from '../../assets/images/Screenshot (36).png'
+import foodLovers from '../../assets/images/Screenshot (4).png'
 import AnimatedLetters from "../AnimatedLetters";
 import "./index.scss";
 
@@ -19,19 +21,29 @@ const Portfolio = () => {
                 </h1>
                 <div className="portfolio-container">
                 <div className="images-container">
-                    <img className="image-box" src={runBuddy} alt="run-buddy website" to=""/>
+                    <a href="https://belcross17.github.io/run-buddy/">
+                    <img className="image-box" src={runBuddy} alt="run-buddy website" />
+                    </a>
                 </div>
                 <div className="images-container">
-                    <img className="image-box" src={runBuddy} alt="run-buddy website" to=""/>
+                    <a href="https://belcross17.github.io/Tourism-App/">
+                    <img className="image-box" src={tourismApp} alt="Tourism app" />
+                    </a>
                 </div>
                 <div className="images-container">
-                    <img className="image-box" src={runBuddy} alt="run-buddy website" to=""/>
+                    <a href="https://food-lovers-project-2.herokuapp.com/">
+                    <img className="image-box" src={foodLovers} alt="Food-Lovers-website-blog" />
+                    </a>
                 </div>
                 <div className="images-container">
-                    <img className="image-box" src={runBuddy} alt="run-buddy website" to=""/>
+                    <a href="https://belcross17.github.io/run-buddy/">
+                    <img className="image-box" src={runBuddy} alt="run-buddy website" />
+                    </a>
                 </div>
                 <div className="images-container">
-                    <img className="image-box" src={runBuddy} alt="run-buddy website" to=""/>
+                    <a href="https://belcross17.github.io/run-buddy/">
+                    <img className="image-box" src={runBuddy} alt="run-buddy website" />
+                    </a>
                 </div>
                 </div>
             </div>
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -2,29 +2,39 @@ import { Link, NavLink } from 'react-router-dom'
 import LogoB from '../../assets/images/logo-b.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope, faHome, faSuitcase, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faClose, faEnvelope, faHome, faSuitcase, faUser } from '@fortawesome/free-solid-svg-icons'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import './index.scss'
+import { useState } from 'react'
 
-const Sidebar = () => (
+const Sidebar = () => {
+    const [showNav, setShowNav] = useState(false);
+
+    return (
     <div className='nav-bar'>
         <Link className='logo' to='/'>
             <img src={LogoB} alt="logo" />
             <img className="sub-logo" src={LogoSubtitle} alt="Brayan Arias" />
         </Link>
-        <nav>
-            <NavLink exact="true" activeclassname="active" className="home-link" to="/">
+        <nav className={showNav ? 'mobile-show' : ''}>
+            <NavLink exact="true" activeclassname="active" className="home-link" onClick={() => setShowNav(false)} to="/">
                 <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
             </NavLink>
-            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+            <NavLink exact="true" activeclassname="active" className="about-link" onClick={() => setShowNav(false)} to="/about">
                 <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
             </NavLink>
-            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+            <NavLink exact="true" activeclassname="active" className="contact-link" onClick={() => setShowNav(false)} to="/contact">
                 <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
             </NavLink>
-            <NavLink exact="true" activeclassname="active" className="portfolio-link" to="/portfolio">
+            <NavLink exact="true" activeclassname="active" className="portfolio-link" onClick={() => setShowNav(false)} to="/portfolio">
                 <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
             </NavLink>
+            <FontAwesomeIcon 
+                onClick={() => setShowNav(false)}
+                icon={faClose}
+                color="#ffd700"
+                size="3x"
+                className='close-icon' />
         </nav>
         <ul>
             <li>
@@ -38,7 +48,14 @@ const Sidebar = () => (
                 </a>
             </li>
         </ul>
+        <FontAwesomeIcon 
+          onClick={() => setShowNav(true)}
+          icon={faBars}
+          color="#ffd700"
+          size="3x"
+          className='hamburger-icon' />
     </div>
     )
+}
 
 export default Sidebar;
