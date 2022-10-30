2952ffd7a37bc34646cad1b44bebdc68d314d7af
Brayan <alfredochabes1@hotmail.com>
2022-10-29 23:21:05 -0400

    removed gsap due to trial version

--- src/components/Home/logo/index.js
+++ src/components/Home/logo/index.js
@@ -1,35 +1,35 @@
 import './index.scss';
 import { useEffect, useRef } from 'react'
 import LogoB from '../../../assets/images/self.JPG'
-import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin';
-import gsap from 'gsap-trial';
+// import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin';
+// import gsap from 'gsap-trial';
 
 
 const Logo = () => {
     const bgRef = useRef()
     const solidLogoRef = useRef()
 
-    useEffect(() => {
-        gsap.registerPlugin(DrawSVGPlugin)
+    // useEffect(() => {
+    //     // gsap.registerPlugin(DrawSVGPlugin)
 
-        gsap
-        .timeline()
-        .to(bgRef.current, {
-            duration: 1,
-            opacity: 1,
-        })
-        gsap.fromTo(
-            solidLogoRef.current,
-            {
-                opacity: 0,
-            },
-            {
-                opacity: 1,
-                delay: 4,
-                duration: 4,
-            }
-        )
-    }, [])
+    // //     gsap
+    // //     .timeline()
+    //     .to(bgRef.current, {
+    //         duration: 1,
+    //         opacity: 1,
+    //     })
+    //     gsap.fromTo(
+    //         solidLogoRef.current,
+    //         {
+    //             opacity: 0,
+    //         },
+    //         {
+    //             opacity: 1,
+    //             delay: 4,
+    //             duration: 4,
+    //         }
+    //     )
+    // }, [])
 
     return (
         <div className='logo-container' ref={bgRef}>
