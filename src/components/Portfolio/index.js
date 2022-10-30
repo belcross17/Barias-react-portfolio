7f2476e150a94e9cd53110953a681a42d7fc72e8
Brayan <alfredochabes1@hotmail.com>
2022-10-29 21:36:24 -0400

    added placeholders for projects

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,3 +1,43 @@
-const Portfolio = () => ('hello');
+import React, { useState } from "react";
+import Loader from "react-loaders";
+import runBuddy from '../../assets/images/run-buddy.png'
+import AnimatedLetters from "../AnimatedLetters";
+import "./index.scss";
+
+
+const Portfolio = () => { 
+    const [letterClass] = useState('text-animate');
+    return (
+        <>
+            <div className="container portfolio-page">
+                <h1 className="page-title">
+                    <AnimatedLetters
+                        letterClass={letterClass}
+                        strArray={"Portfolio".split("")}
+                        idx={15}
+                    />
+                </h1>
+                <div className="portfolio-container">
+                <div className="images-container">
+                    <img className="image-box" src={runBuddy} alt="run-buddy website" to=""/>
+                </div>
+                <div className="images-container">
+                    <img className="image-box" src={runBuddy} alt="run-buddy website" to=""/>
+                </div>
+                <div className="images-container">
+                    <img className="image-box" src={runBuddy} alt="run-buddy website" to=""/>
+                </div>
+                <div className="images-container">
+                    <img className="image-box" src={runBuddy} alt="run-buddy website" to=""/>
+                </div>
+                <div className="images-container">
+                    <img className="image-box" src={runBuddy} alt="run-buddy website" to=""/>
+                </div>
+                </div>
+            </div>
+            <Loader type="pacman" />
+        </>
+    );
+}
 
 export default Portfolio;
