e1dc64343ab93aa37bc7c1dea8c7fa5ce1000fc7
Brayan <alfredochabes1@hotmail.com>
2022-10-29 20:03:31 -0400

    added map to contact me to show my possible location

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import Loader from 'react-loaders';
 import AnimatedLetters from '../AnimatedLetters';
 import './index.scss';
@@ -20,7 +21,7 @@ const Contact = () => {
                     <p>
                         I am interested in freelance  opportunities, especially ambitious or Frontend projects.
                         However, if you have any questions or possible challenges, please don't hesitate to contact me using 
-                        the information below.
+                        the information below or send me your info plus a message and I will get back to you as soon as possible.
                     </p>
                     <br />
                     <ul>
@@ -31,6 +32,46 @@ const Contact = () => {
                             Phone Number: (786)-234-5421
                         </li>
                     </ul>
+
+                    <div className='contact-form'>
+                        <form>
+                            <ul>
+                                <li className='half'>
+                                    <input type="text" name="name" placeholder="Name" required />
+                                </li>
+                                <li className='half'>
+                                    <input type="email" name="email" placeholder="Email" required />
+                                </li>
+                                <li>
+                                    <input placeholder="Subject" type="text" name="subject" required/>
+                                </li>
+                                <li>
+                                    <textarea placeholder="Message" name="Message" required/>
+                                </li>
+                                <li>
+                                    <input className='flat-button' type="submit" value="SEND"/>
+                                </li>
+                            </ul>
+                        </form>
+                    </div>
+                </div>
+                <div className='info-map'>
+                    Brayan Arias
+                    <br />
+                    United States,
+                    <br />
+                    1901 South river Dr <br />
+                    Miami <br />
+                    <span>alfredochabes1@hotmail.com</span>
+
+                </div>
+                <div className='map-wrap'>
+                    <MapContainer center={[25.798, -80.13339]} zoom= {13}>
+                        <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
+                        <Marker position={[25.798, -80.13339]}>
+                            <Popup>The Sunshine city</Popup>
+                        </Marker>
+                    </MapContainer>
                 </div>
             </div>
             <Loader type='pacman' />
